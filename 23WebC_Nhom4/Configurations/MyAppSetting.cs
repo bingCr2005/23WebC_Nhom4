26c386c8fc15b4d055cf3c3d07242c66992a707f
BaoNg11 <baonguyenqn1204@gmail.com>
2025-09-21 00:44:03 +0700

    Upload thư mục Configurations

---
+++ 23WebC_Nhom4/Configurations/MyAppSetting.cs
@@ -0,0 +1,8 @@
+﻿namespace th_nhom01.Configurations
+{
+    public class MyAppSetting
+    {
+        public long MaxFileSize { get; set; }
+        public List<string> BannedIPs { get; set; } = new List<string>();
+    }
+}
