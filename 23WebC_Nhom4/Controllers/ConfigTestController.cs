943c6f6c6140752ecd76c60b52967b401caca4b9
BaoNg11 <baonguyenqn1204@gmail.com>
2025-09-21 00:53:17 +0700

    Thêm ConfigTestController.cs

---
+++ 23WebC_Nhom4/Controllers/ConfigTestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using th_nhom01.Configurations;
+
+namespace th_nhom01.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ConfigTestController : ControllerBase
+    {
+        private readonly MyAppSetting _settings;
+
+        public ConfigTestController(IOptions<MyAppSetting> settings)
+        {
+            _settings = settings.Value;
+        }
+
+        [HttpGet("maxfilesize")]
+        public IActionResult GetMaxFileSize()
+        {
+            return Ok($"MaxFileSize = {_settings.MaxFileSize}");
+        }
+
+        [HttpGet("bannedips")]
+        public IActionResult GetBannedIPs()
+        {
+            return Ok(_settings.BannedIPs);
+        }
+    }
+}
