665ec53243d89748b97bf8e7b491b5e734e3f728
Trần Hồng Kỳ <0306231303@caothang.edu.vn>
2025-09-21 02:01:29 +0700

    Xây dựng Middleware RequestLog.cs để lưu URL, thời gian nhận, IP vào request.log

--- 23WebC_Nhom4/Middleware/RequestLog.cs
+++ 23WebC_Nhom4/Middleware/RequestLog.cs
@@ -2,5 +2,25 @@
 {
     public class RequestLog
     {
+        private readonly RequestDelegate _next;
+
+        public RequestLog(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            string method = context.Request.Method;
+            string url = context.Request.Path + context.Request.QueryString;
+            string ip = context.Connection.RemoteIpAddress != null ? context.Connection.RemoteIpAddress.ToString() : "Unknown";
+
+            string log = $"{time} | {method} | {url} | {ip}\n";
+
+            await File.AppendAllTextAsync("request.log", log);
+
+            await _next(context);
+        }
     }
 }
